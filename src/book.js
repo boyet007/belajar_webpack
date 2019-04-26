c47280b481337289261f14722902556fc439dd25
Philip Loa <boyet007@gmail.com>
2019-04-26 16:52:04 +0700

    lanjut dirumah

---
+++ src/book.js
@@ -0,0 +1,8 @@
+export default class Book {
+    constructor(title) {
+        this.title = title
+    }
+    getTitle() {
+        return this.title
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import Book from './book'
+
+const book = new Book('Majalah bobo5')
+document.write(book.getTitle())
